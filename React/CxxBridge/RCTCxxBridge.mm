d485e41e8edd249295e8500ec37c674699ad2c9f
Stanisław Chmiela <sjchmiela@gmail.com>
2020-08-20 19:05:55 +0200

    Make RCTCxxBridgeDelegate not care about return value of jsExecutorFactoryForBridge

--- React/CxxBridge/RCTCxxBridge.mm
+++ React/CxxBridge/RCTCxxBridge.mm
@@ -336,7 +336,7 @@ struct RCTInstanceCallback : public InstanceCallback {
   if (!self.executorClass) {
     if ([self.delegate conformsToProtocol:@protocol(RCTCxxBridgeDelegate)]) {
       id<RCTCxxBridgeDelegate> cxxDelegate = (id<RCTCxxBridgeDelegate>)self.delegate;
-      executorFactory = [cxxDelegate jsExecutorFactoryForBridge:self];
+      executorFactory = std::make_shared<JSCExecutorFactory>(*reinterpret_cast<JSCExecutorFactory *>([cxxDelegate jsExecutorFactoryForBridge:self]));
     }
     if (!executorFactory) {
       executorFactory = std::make_shared<JSCExecutorFactory>(nullptr);
